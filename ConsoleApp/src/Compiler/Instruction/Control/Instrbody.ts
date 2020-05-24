b3b88b5029fd4233a99ca7ed8470e532da7eb9d8
erflod5 <2883388711710@ingenieria.usac.edu.gt>
2020-05-24 08:26:26 -0600

    Principal statement

---
+++ ConsoleApp/src/Compiler/Instruction/Control/Instrbody.ts
@@ -0,0 +1,23 @@
+import { Instruction } from "../../Abstract/Instruction";
+import { Enviorement } from "../../SymbolTable/Enviorement";
+import { Generator } from "../../Generator/Generator";
+
+export class Instrbody extends Instruction {
+    private instructions: Array<Instruction> | null;
+
+    constructor(instructions: Array<Instruction> | null, line: number, column: number) {
+        super(line, column);
+        this.instructions = instructions;
+    }
+
+    compile(enviorement: Enviorement): any {
+        const newEnv = new Enviorement(enviorement);
+        this.instructions?.forEach((instruction)=>{
+            try {
+                instruction.compile(newEnv);
+            } catch (error) {
+                console.log(error);
+            }
+        });
+    }
+}
