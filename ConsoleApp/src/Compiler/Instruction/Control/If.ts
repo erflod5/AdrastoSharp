659b44a8459249e83744cf49e48fc566b19b54a2
erflod5 <2883388711710@ingenieria.usac.edu.gt>
2020-05-24 07:59:23 -0600

    If statement

---
+++ ConsoleApp/src/Compiler/Instruction/Control/If.ts
@@ -0,0 +1,45 @@
+import { Instruction } from "../../Abstract/Instruction";
+import { Expression } from "../../Abstract/Expression";
+import { Enviorement } from "../../SymbolTable/Enviorement";
+import { Types } from "../../Utils/Type";
+import { Error } from "../../Utils/Error";
+import { Generator } from "../../Generator/Generator";
+
+export class If extends Instruction {
+    private condition: Expression | null;
+    private instruction: Instruction;
+    private elseI: Instruction | null;
+
+    constructor(condition: Expression | null, instruction: Instruction, elseI: Instruction | null, line: number, column: number) {
+        super(line, column);
+        this.condition = condition;
+        this.instruction = instruction;
+        this.elseI = elseI;
+    }
+
+    compile(enviorement: Enviorement) : void{
+        const condition = this.condition?.compile(enviorement);
+        const generator = Generator.getInstance();
+        const newEnv = new Enviorement(enviorement);
+        if(condition == undefined){
+            this.instruction.compile(newEnv);
+            return;
+        }
+        else if(condition.type.type == Types.BOOLEAN){
+            generator.addLabel(condition.trueLabel);
+            this.instruction.compile(newEnv);
+            if(this.elseI != null){
+                const tempLbl = generator.newLabel();
+                generator.addGoto(tempLbl);
+                generator.addLabel(condition.falseLabel);
+                this.elseI.compile(enviorement);
+                generator.addLabel(tempLbl);
+            }
+            else{
+                generator.addLabel(condition.falseLabel);
+            }
+            return;
+        }
+        throw new Error(this.line,this.column,'Semantico',`La condicion no es booleana: ${condition?.type.type}`);
+    }
+}
